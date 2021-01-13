772817db9f40f629d2ed6c4257c49433efc77f36
Nicolas Tessore <n.tessore@ucl.ac.uk>
2021-01-13 17:35:05 +0000

    Shen et al. (2004): work in progress on Fig. 4

--- sources/shen_et_al_2003/fig_1_4.py
+++ sources/shen_et_al_2003/fig_1_4.py
@@ -4,6 +4,7 @@ from matplotlib.ticker import FormatStrFormatter, MultipleLocator
 import io
 
 from astroquery.sdss import SDSS
+from astropy.cosmology import FlatLambdaCDM
 
 
 # SDSS sample
@@ -14,6 +15,7 @@ SELECT TOP 50000
     p.objID as id,
     p.petroR50_r AS R50_r,
     p.petroMag_r-p.extinction_r AS r,
+    p.petroR90_r/p.petroR50_r AS c,
     s.z AS z
 FROM
     PhotoObj AS p
@@ -46,14 +48,27 @@ Ngal = len(result)
 
 print('selected %d galaxies' % Ngal)
 
-R50_r, r, z = result['R50_r'], result['r'], result['z']
+R50_r, r, c, z = result['R50_r'], result['r'], result['c'], result['z']
 mu50_r = r + 2.5*np.log10(2*np.pi*R50_r**2)
+
+
+# cosmology
+# =========
+
+cosmo = FlatLambdaCDM(Om0=0.3, H0=0.7)
+
+print(f'cosmology: {cosmo}')
+
+R50_r_phys = R50_r/60 * cosmo.kpc_proper_per_arcmin(z).value
+M_r = r - cosmo.distmod(z).value + 5
 
 
 # figures
 # =======
 
 # Fig. 1
+# ------
+
 fig, ax = plt.subplots(2, 2, figsize=(4.0, 4.0), dpi=600)
 
 ax[0, 0].hist(R50_r, range=[0, 12], bins=30, histtype='step', ec='k', lw=0.5,
@@ -132,3 +147,39 @@ fig.subplots_adjust(wspace=0.55, hspace=0.55)
 
 fig.savefig('shen_et_al_2003_fig_1.pdf', bbox_inches='tight')
 fig.savefig('shen_et_al_2003_fig_1.svg', bbox_inches='tight')
+
+plt.close()
+
+
+# Fig. 4
+# ------
+
+M_bins_late = np.linspace(-15.9, -23.9, 17)
+M_bins_early = np.linspace(-18.5, -24, 14)
+
+M_index = np.empty(len(M_r), dtype=int)
+M_index[c < 2.86] = np.digitize(M_r[c < 2.86], M_bins_late)
+M_index[c > 2.86] = np.digitize(M_r[c > 2.86], M_bins_early)
+
+R50_bar_late = np.empty(len(M_bins_late)-1)
+R50_bar_early = np.empty(len(M_bins_early)-1)
+
+for i in range(1, len(M_bins_late)):
+    R50_in_bin = R50_r_phys[(c < 2.86) & (M_index == i)]
+    R50_bar_late[i-1] = np.median(R50_in_bin)
+
+for i in range(1, len(M_bins_early)):
+    R50_in_bin = R50_r_phys[(c > 2.86) & (M_index == i)]
+    R50_bar_early[i-1] = np.median(R50_in_bin)
+
+ax = plt.subplot(111)
+
+ax.semilogy((M_bins_late[:-1]+M_bins_late[1:])/2, R50_bar_late, 'v', mfc='none')
+ax.semilogy((M_bins_early[:-1]+M_bins_early[1:])/2, R50_bar_early, 's', mfc='none')
+
+ax.set_xlim(-15.5, -24.5)
+
+ax.xaxis.set_major_locator(MultipleLocator(2))
+ax.xaxis.set_minor_locator(MultipleLocator(0.5))
+
+plt.show()
